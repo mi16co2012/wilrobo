97404967193f3200b6683b824a51070f79f78eae
mi16co2012 <www.mi16co.com@gmail.com>
2021-03-28 15:13:30 +0800

    1st upload
    adding all the files

---
+++ src/robots.js
@@ -0,0 +1,73 @@
+export const robots = [
+    {
+        id:1,
+        name:'WIlson Co',
+        username: 'kiracomi',
+        email: 'wilson.co@gmail.com'
+        
+    },
+    {
+        id:2,
+        name:'Jr Co',
+        username: 'jrco',
+        email: 'killer.co@gmail.com'
+        
+    },
+    {
+        id:3,
+        name:'Roy Co',
+        username: 'royco',
+        email: 'roy.co@gmail.com'
+
+    },
+    {
+        id:4,
+        name:'Roy Co',
+        username: 'royco',
+        email: 'roy.co@gmail.com'
+
+    },
+    {
+        id:5,
+        name:'Roy Co',
+        username: 'royco',
+        email: 'roy.co@gmail.com'
+
+    },
+    {
+        id:6,
+        name:'Roy Co',
+        username: 'royco',
+        email: 'roy.co@gmail.com'
+
+    },
+    {
+        id:7,
+        name:'Roy Co',
+        username: 'royco',
+        email: 'roy.co@gmail.com'
+
+    },
+    {
+        id:8,
+        name:'Roy Co',
+        username: 'royco',
+        email: 'roy.co@gmail.com'
+
+    },
+    {
+        id:9,
+        name:'Roy Co',
+        username: 'royco',
+        email: 'roy.co@gmail.com'
+
+    },
+    {
+        id:10,
+        name:'Roy Co',
+        username: 'royco',
+        email: 'roy.co@gmail.com'
+
+    },
+
+]
